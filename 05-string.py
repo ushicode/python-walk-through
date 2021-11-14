b5fe7bc24fd12927e95225ca62001b63c31c33b9
Collins Ushi <collinsushi@gmail.com>
2021-11-14 02:21:44 +0000

    Manipulate strings with built in functions

---
+++ 05-string.py
@@ -0,0 +1,8 @@
+# Use built in functions to manipulate string
+fullname = 'Ushi Collins'
+
+print(fullname.upper())  # Switch to uppercase
+print(fullname.replace('Collins', 'code'))  # replace a string
+print(len(fullname))  # Display length
+print("Ushi" in fullname)  # Confirm if Ushi is exist in fullname
+print("Ushi" not in fullname)  # Confirm if Ushi is not in fullname
