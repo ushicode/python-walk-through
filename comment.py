2a6f50cada323195c22037a64bcfb6b37fdb544d
Collins Ushi <collinsushi@gmail.com>
2021-11-14 01:56:29 +0000

    Add comment

---
+++ comment.py
@@ -0,0 +1,8 @@
+# This is a single line python comment
+
+"""
+This is a multi-line python comment.
+N.B python comments are not executed
+at run time.
+Hmmm! python codes are executed at runtime,
+"""
