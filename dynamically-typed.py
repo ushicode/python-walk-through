a1cedfd2ca00520172e2d15fd2949cc8da48c6e0
Collins Ushi <collinsushi@gmail.com>
2021-11-14 01:50:15 +0000

    Add dynamically-typed sample

---
+++ dynamically-typed.py
@@ -0,0 +1,5 @@
+# Python is Dynamically typed. Thus no point specifying var type
+# However, to be explicit, go thus;
+carBrand: str = "Tesla"
+vehicleLicense: int = 7942
+carSalvageValue: float = 160000.78
