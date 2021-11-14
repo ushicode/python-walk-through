0954fbf04f304f899592f2cc1693e89f8c1e6774
Collins Ushi <collinsushi@gmail.com>
2021-11-14 00:32:58 +0000

    Using variables

---
+++ variable.py
@@ -0,0 +1,9 @@
+firstName = "Collins"
+age = 20
+pi = 3.1427
+number = [1, 2, 3, 4, 5]
+
+# Declaring variables in one line
+height, race = 1.80, "Black"
+
+print(firstName + " is " + str(height) + "m, and he is " + race)
